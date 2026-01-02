5021abd378963295db136307504fb6bf9737a0d0
NarendranathCh <149961832+NarendranathCh@users.noreply.github.com>
2026-01-02 10:15:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,35 +1,34 @@
 class Solution {
-  
     static void sort012(int[] arr) {
         int n = arr.length;
-        int c0 = 0, c1 = 0, c2 = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) 
-                c0 += 1;
-            else if (arr[i] == 1) 
-                c1 += 1;
-            else 
-                c2 += 1;
+        int lo = 0;
+        int hi = n - 1;
+        int mid = 0;
+        
+        while (mid <= hi) {
+            if (arr[mid] == 0) {
+                int temp = arr[lo];
+                arr[lo++] = arr[mid];
+                arr[mid++] = temp;
+            }
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+            else {
+                int temp = arr[mid];
+                arr[mid] = arr[hi];
+                arr[hi--] = temp;
+            }
         }
-
-        int idx = 0;
-      
-        for (int i = 0; i < c0; i++)
-            arr[idx++] = 0;
-        for (int i = 0; i < c1; i++)
-            arr[idx++] = 1;
-        for (int i = 0; i < c2; i++)
-            arr[idx++] = 2;
     }
-  
-    public static void main(String[] args) {
-        int[] a = { 0, 1, 2, 0, 1, 2 };
-        int n = a.length;
 
-        sort012(a);
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 2, 0, 1, 2 };
+        int n = arr.length;
+
+        sort012(arr);
 
         for (int i = 0; i < n; i++)
-            System.out.print(a[i] + " ");
+            System.out.print(arr[i] + " ");
     }
-}
+}          
